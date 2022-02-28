16d31312f1ed06bd9513c31d7047ec7c27359bd5
jptalukdar <jyotiplaban@gmail.com>
2022-02-28 11:15:06 +0530

    added basic api

--- whattowatch/apiserver/views.py
+++ whattowatch/apiserver/views.py
@@ -1,3 +1,13 @@
+import imp
 from django.shortcuts import render
-
+from django.http import JsonResponse
+import json
 # Create your views here.
+
+def getMovie(request):
+  response = {
+    "Movie" : "The silent voice",
+    "Category" : "Anime",
+    "Year" : 2016
+  }
+  return JsonResponse(response)
